f370dd7d65c2b8c5d649a5173a4a5bc3fd75a856
dquispeott <dvd2qo@gmail.com>
2017-05-15 16:22:08 -0700

    initial checkin

---
+++ src/main/java/com/example/jmsconnect/Application.java
@@ -0,0 +1,15 @@
+package com.example.jmsconnect;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
